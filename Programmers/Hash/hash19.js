6b4139d1c47fd362e9725209f0ac3c0bb1e0cb80
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-09-05 21:56:51 +0900

    hash 2번 문제(문제19)

---
+++ Programmers/Hash/hash19.js
@@ -0,0 +1,25 @@
+// stringList => 문자열 리스트
+// queryList => 쿼리 리스트
+// 각 쿼리 리스트에 있는 문자열이 stringList의 문자열 리스트에 있는지 여부를 확인
+// 문자열이 있으면 true, 없으면 false
+
+function solution(stringList, queryList) {
+  const hash = new Map();
+  const result = [];
+  for (let i = 0; i < stringList.length; i++) {
+    hash.set(stringList[i], i);
+  }
+
+  for (let i = 0; i < queryList.length; i++) {
+    if (hash.has(queryList[i])) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+  return result;
+}
+
+const stringList = ["apple", "banana", "cherry"];
+const queryList = ["banana", "kiwi", "melon", "apple"];
+console.log(solution(stringList, queryList)); // [True,false,false,True]
